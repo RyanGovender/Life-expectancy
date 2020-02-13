7d49b73c327e5a36e9b2a7447bd574c39c819e61
ryangovender <21614349@dut4life.ac.za>
2020-02-13 11:18:51 +0200

    Add project files.

---
+++ Life expectancy/Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Life_expectancy.Models
+{
+    public abstract class Question
+    {
+        public int Id { get; set; }
+        public string QuestionToAsk { get; set; }
+
+        public Question(int id,string question)
+        {
+            Id = id;
+            QuestionToAsk = question;
+        }
+
+    }
+}
---
+++ Life expectancy/Models/StringQuestion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Life_expectancy.Models
+{
+   public class StringQuestion: Question
+    {
+        public List<string> ListOfPossibleAnswers { get; set; }
+
+        public StringQuestion(int id,string question,List<string> possibleAnswers):base(id,question)
+        {
+            ListOfPossibleAnswers = possibleAnswers;
+        }
+    }
+}
